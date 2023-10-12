99402a01be829b5a5e2cb5759af773f7455372b8
W0785530 <W0785530@ADL-A1201I-06>
2023-10-12 14:21:15 -0400

    Creation of a Data folder and MvcBalloonContext.cs Class

---
+++ MvcBalloon/Data/MvcBalloonContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcBalloon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Creation of the MvcBalloonContext Class
+
+namespace MvcBalloon.Data
+{
+    public class MvcBalloonContext : DbContext
+
+    {
+        public MvcBalloonContext(DbContextOptions<MvcBalloonContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<Balloon> Balloon { get; set; }
+    }
+}
