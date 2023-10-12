a32fd266ef6694518b67f09b8839c0fecd39ccd1
W0785530 <W0785530@ADL-A1201I-06>
2023-10-12 14:24:39 -0400

    Modification of Startup.cs and appsettings.json

--- MvcBalloon/Startup.cs
+++ MvcBalloon/Startup.cs
@@ -1,3 +1,5 @@
+using MvcBalloon.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,9 @@ namespace MvcBalloon
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcBalloonContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MvcBalloonContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
